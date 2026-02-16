bc741901304f59ba97070b8f402682e5e2fc1d53
FANIMAN <fantahunfekadu1@gmail.com>
2026-02-16 14:39:30 +0300

    feat: implement subcity management

---
+++ internal/repository/interfaces/subcity_repository.go
@@ -0,0 +1,15 @@
+package interfaces
+
+import (
+	"context"
+
+	"github.com/FANIMAN/housing-lottery/internal/domain"
+)
+
+type SubcityRepository interface {
+	Create(ctx context.Context, subcity *domain.Subcity) error
+	GetByID(ctx context.Context, id string) (*domain.Subcity, error)
+	GetAll(ctx context.Context) ([]*domain.Subcity, error)
+	Update(ctx context.Context, subcity *domain.Subcity) error
+	Delete(ctx context.Context, id string) error
+}
